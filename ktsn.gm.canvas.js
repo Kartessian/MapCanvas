0638e4febdd27b2d83ca0125b525de2c8f35d5de
Javier Chaos <jachaos@gmail.com>
2014-08-11 15:19:10 -0500

    Added common drawing functions

--- ktsn.gm.canvas.js
+++ ktsn.gm.canvas.js
@@ -3,10 +3,6 @@ canvasLayer.prototype.draw = function () { };
 canvasLayer.prototype.onAdd = function () {
     var pane = this.getPanes().overlayLayer;
 
-    this._canvas = document.createElement("canvas");
-    this._canvas.style.position = 'absolute';
-    pane.appendChild(this._canvas);
-
     // add the canvas for each layer (if any)
     for (var i = 0, len = K._layers.length; i < len; i++) {
         var canvas = document.createElement("canvas");
@@ -40,6 +36,7 @@ canvasLayer.prototype.onRemove = function () {
     this._click = null;
 
 };
+
 function canvasLayer(map) {
     this.setMap(map);
 }
@@ -49,6 +46,30 @@ function handler(t, fn) {
         fn(t);
     }
 }
+
+function getNormalizedCoord(coord, zoom) {
+    var y = coord.y;
+    var x = coord.x;
+
+    // tile range in one direction range is dependent on zoom level
+    // 0 = 1 tile, 1 = 2 tiles, 2 = 4 tiles, 3 = 8 tiles, etc
+    var tileRange = 1 << zoom;
+
+    // don't repeat across y-axis (vertically)
+    if (y < 0 || y >= tileRange) {
+        return null;
+    }
+
+    // repeat across x-axis
+    if (x < 0 || x >= tileRange) {
+        x = (x % tileRange + tileRange) % tileRange;
+    }
+
+    return {
+        x: x,
+        y: y
+    };
+};
 
 window.K = {
     init: function (options) {
@@ -60,6 +81,7 @@ window.K = {
 
         this._pane = null;
         this._layers = [];
+        this._tiles = [];
 
         this._base = new canvasLayer(this._map);
 
@@ -77,12 +99,28 @@ window.K = {
         return this._layers.length;
     },
 
+    // experimental
+    addTile: function (tile) {
+        this._tiles.push(tile);
+
+        this._map.overlayMapTypes.insertAt(0, tile);
+
+        return this._tiles.length;
+    },
+
     destroy: function () {
         // basic detroy implementation, needs further consideration
         K._base.setMap(null);
         K._base = null;
         K._layers = [];
         K._map = null;
+    },
+
+    clean: function () {
+        for (var i = 0, len = K._layers.length; i < len; i++) {
+            K._pane.removeChild(K._layers[i]._canvas);
+        }
+        K._layers = [];
     },
 
     removeLayer: function (layerId) {
@@ -92,12 +130,47 @@ window.K = {
         this._layers.splice(layerId, 1);
     },
 
+    // experimental
+    removetile: function (tileId) {
+
+        // remove the tile
+        this._map.overlayMapTypes.removeAt(ix);
+
+        // remove the layer from the array
+        this._tiles.splice(tileId, 1);
+    },
+
+    showInfoBox: function (html, geo, yoffset) {
+        if (this._infobox != null) {
+            this._infobox.setMap(null);
+        }
+
+        if (yoffset < 7) yoffset = 7;
+        yoffset += 12;
+
+        this._infobox = new InfoBox({
+            boxClass: 'infobox',
+            content: '<div>' + html + '</div>',
+            position: geo,
+            disableAutoPan: false,
+            pixelOffset: new google.maps.Size(0, yoffset),
+            zIndex: null,
+            closeBoxMargin: "12px 4px 2px 2px",
+            closeBoxURL: "http://www.google.com/intl/en_us/mapfiles/close.gif",
+            infoBoxClearance: new google.maps.Size(1, 1),
+            alignBottom: true
+        });
+
+        this._infobox.open(this._map);
+    },
+
     // constructors
 
     Layer: function (name, style) {
         this.points = [];
         this.style = style;
         this.visible = true;
+        this.type = 'layer';
 
         this.name = name;
 
@@ -122,6 +195,33 @@ window.K = {
             }
             return this;
         };
+
+        this.onClick = function (point) {
+            // default handler for the click event
+            // will show an infobox with the available attributes
+            var html = '';
+            for (var prop in point) {
+                html += '<p><b>' + prop + ':</b> ' + point[prop] + '</p>';
+            }
+
+            K.showInfoBox(html, point.geo, this.style.pointSize / 2);
+        };
+    },
+
+    // experimental -- the tiles should be loaded in the canvas...
+    // instead of using the maptype functionallity of google maps 
+    Tile: function (tileName, ds) {
+        var tile = new google.maps.ImageMapType({
+            getTileUrl: function (coord, zoom) {
+                coord = getNormalizedCoord(coord, zoom);
+                return '/tiles/' + zoom + '/' + coord.x + '/' + coord.y + '/' + ds;
+            },
+            tileSize: new google.maps.Size(256, 256),
+            name: name
+        });
+        tile.type = 'tile';
+
+        return tile;
     },
 
     Point: function (lat, lng, properties) {
@@ -137,19 +237,38 @@ window.K = {
     _click: function (e) {
         if (this._layerOver < 0) return;
 
-        var layer = K._layers[this._layerOver];
+        var layer = K._layers[this._layerOver],
+            x = e.latLng.lat(),
+            y = e.latLng.lng(),
+            minDist = layer.style.pointSize * layer.style.pointSize,
+            point = null;
 
         // find the closest point to the current location
-        // pending: try some other way to find the point without going through all the array
-        // maybe ordering the array in some way (like distance to 0,0) 
         for (var i = 0, len = layer.points.length; i < len; i++) {
-            var geo = layer.points[i];
-            // to be continued ...
+            var p = layer.points[i],
+                xpos = p.geo.lat(),
+                ypos = p.geo.lng(),
+                dist = (x - xpos) * (x - xpos) + (y - ypos) * (y - ypos);
+
+            if (dist < minDist) {
+                minDist = dist;
+                point = p;
+
+                // if the distance is 0, we cannot get closer
+                if (dist == 0) {
+                    break;
+                }
+            }
+        }
+
+        if (point != null) {
+            layer.onClick(point);
         }
 
     },
 
     _draw: function (t) {
+
         var overlayProjection = t.getProjection()
 
         if (overlayProjection === undefined) {
@@ -239,8 +358,7 @@ window.K = {
             adjust = pointSize / 2,
             adjustPow = adjust * adjust,
             alphaSize = style.alpha,
-            //colors = this.colors_ || drawing.getGradientPow(this.fillColor, this.borderColor, pointSize),
-            colors = drawing.getGradientPow(style.fillColor, style.borderColor, pointSize),
+            colors = K.drawing.getGradientPow(style.fillColor, style.borderColor, pointSize),
             swx = sw.x,
             ney = ne.y,
             maxPoint = 0,
@@ -289,7 +407,7 @@ window.K = {
 
         }
 
-        var pattern = drawing.createPattern([{ r: 0, g: 0, b: 255 }, { r: 0, g: 255, b: 255 }, { r: 0, g: 255, b: 0 }, { r: 255, g: 255, b: 0 }, { r: 255, g: 0, b: 0 }], maxPoint);
+        var pattern = K.drawing.createPattern([{ r: 0, g: 0, b: 255 }, { r: 0, g: 255, b: 255 }, { r: 0, g: 255, b: 0 }, { r: 255, g: 255, b: 0 }, { r: 255, g: 0, b: 0 }], maxPoint);
 
         points = canvasHeight;
         while (points--) {
@@ -340,8 +458,7 @@ window.K = {
             adjust = pointSize / 2,
             adjustPow = adjust * adjust,
             alphaSize = style.alpha,
-            //colors = this.colors_ || drawing.getGradientPow(this.fillColor, this.borderColor, pointSize),
-            colors = drawing.getGradientPow(style.fillColor, style.borderColor, pointSize),
+            colors = K.drawing.getGradientPow(style.fillColor, style.borderColor, pointSize),
             swx = sw.x,
             ney = ne.y;
 
@@ -416,7 +533,7 @@ window.K = {
         }
 
         /* the template point - will be copied everywhere a new point needs to be placed */
-        var dot = drawing.createDot(style.type, style.fillColor.hex, style.borderColor.hex, pointSize, style.alpha);
+        var dot = K.drawing.createDot(style.type, style.fillColor.hex, style.borderColor.hex, pointSize, style.alpha);
 
         /* adjust to the position of the point as
             the center of the littlepoint should be actually the
@@ -479,6 +596,177 @@ window.K = {
             }
         }
 
-    }
+    },
 
+    drawing: {
+        getGradient: function (initialColor, finalColor, pointSize) {
+
+            if (typeof initialColor == 'string') {
+                initialColor = this.hexToRgb(initialColor);
+            }
+
+            if (typeof finalColor == 'string') {
+                finalColor = this.hexToRgb(finalColor);
+            }
+
+            var steps = Math.round(pointSize / 2),
+                rStep = (finalColor.r - initialColor.r) / steps,
+                gStep = (finalColor.g - initialColor.g) / steps,
+                bStep = (finalColor.b - initialColor.b) / steps,
+                colors = [];
+
+            for (var i = 0; i <= steps; i++) {
+                var r = Math.round(initialColor.r + (rStep * i)),
+                    g = Math.round(initialColor.g + (gStep * i)),
+                    b = Math.round(initialColor.b + (bStep * i));
+
+                colors.push({ "r": r, "g": g, "b": b, "hex": '#' + this.toHex(r) + this.toHex(g) + this.toHex(b) });
+            }
+
+            return colors;
+        },
+
+        getGradientPow: function (fillColor, endColor, pointSize) {
+            var steps = Math.round(pointSize * pointSize / 4);
+
+            var rStep = (endColor.r - fillColor.r) / steps;
+            var gStep = (endColor.g - fillColor.g) / steps;
+            var bStep = (endColor.b - fillColor.b) / steps;
+
+            var colors = [];
+
+            for (var i = 0; i <= steps; i++) {
+                var r = Math.round(fillColor.r + (rStep * i));
+                var g = Math.round(fillColor.g + (gStep * i));
+                var b = Math.round(fillColor.b + (bStep * i));
+
+                colors.push({ "r": r, "g": g, "b": b, "hex": '#' + this.toHex(r) + this.toHex(g) + this.toHex(b) });
+            }
+
+            return colors;
+        },
+
+        createPattern: function (colors, count) {
+            var pattern = [], steps = Math.round(count / (colors.length - 1));
+
+            for (var i = 0, len = colors.length - 1 ; i < len; i++) {
+
+                var initialColor = colors[i],
+                    finalColor = colors[i + 1],
+                    rStep = (finalColor.r - initialColor.r) / steps,
+                    gStep = (finalColor.g - initialColor.g) / steps,
+                    bStep = (finalColor.b - initialColor.b) / steps;
+
+                for (var j = 0 ; j <= steps; j++) {
+                    var r = Math.round(initialColor.r + (rStep * j)),
+                        g = Math.round(initialColor.g + (gStep * j)),
+                        b = Math.round(initialColor.b + (bStep * j));
+
+                    pattern.push({ "r": r, "g": g, "b": b });
+                }
+
+            }
+
+            var fC = colors[colors.length - 1];
+            while (pattern.length < count) {
+                pattern.push(fC);
+            }
+
+            return pattern;
+        },
+
+        toHex: function (num) {
+            num = num.toString(16);
+            if (num.length == 1) num = "0" + num;
+            return num;
+        },
+
+        hexToRgb: function (hex) {
+            var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+            return result ? {
+                r: parseInt(result[1], 16),
+                g: parseInt(result[2], 16),
+                b: parseInt(result[3], 16),
+                hex: hex
+            } : null;
+        },
+
+        invertColor: function (color) {
+            if (color[0] == '#') {
+                color = color.substring(1);           // remove #
+            }
+            color = parseInt(color, 16);          // convert to integer
+            color = 0xFFFFFF ^ color;             // invert three bytes
+            color = color.toString(16);           // convert to hex
+            color = ("000000" + color).slice(-6); // pad with leading zeros
+            color = "#" + color;                  // prepend #
+            return color;
+        },
+
+        createDot: function (type, fillColor, borderColor, size, alpha, steps) {
+            /// <param name="type">dot type: simple, shadow</param>
+            /// <param name="fillColor">dot inner color</param>
+            /// <param name="borderColor">dot border color</param>
+            /// <param name="size">total diameter of the dot</param>
+            /// <param name="alpha">max alpha value</param>
+            /// <param name="steps">number of steps for scatter dots</param>
+
+            if (type == 'scatter') {
+                if (steps > 1) {
+                    return this.createScatterDot(fillColor, borderColor, size, alpha, steps);
+                } else {
+                    type = 'plain';
+                }
+            }
+
+            var canvas = document.createElement("canvas");
+            canvas.width = size;
+            canvas.height = size;
+
+            var context = canvas.getContext("2d");
+
+            switch (type) {
+                case 'shadow':
+                    // firt draw the shadow
+                    context.globalAlpha = (alpha / 255) / 2;
+                    context.beginPath();
+                    context.arc(size / 2, size / 2, size / 2, 0, 2 * Math.PI, false);
+                    context.fillStyle = fillColor;
+                    context.fill();
+                    // then place the dot
+                    context.globalAlpha = alpha / 255;
+                    context.beginPath();
+                    context.arc(size / 2, size / 2, size / 4, 0, 2 * Math.PI, false);
+                    context.fillStyle = fillColor;
+                    context.fill();
+                    context.lineWidth = 1;
+                    context.strokeStyle = borderColor;
+                    context.stroke();
+                    break;
+                case 'plain':
+                    context.globalAlpha = alpha / 255;
+                    context.beginPath();
+                    context.arc(size / 2, size / 2, size / 2, 0, 2 * Math.PI, false);
+                    context.fillStyle = fillColor;
+                    context.fill();
+                    context.lineWidth = 1;
+                    context.strokeStyle = borderColor;
+                    context.stroke();
+                    break;
+            }
+
+            return canvas;
+        },
+
+        createScatterDot: function (initialColor, finalColor, size, alpha, steps) {
+
+            var colors = this.getGradient(initialColor, finalColor, steps * 2),
+                points = [];
+            for (var i = 0, len = colors.length; i < len; i++) {
+                points.push(this.createDot('plain', colors[i].hex, this.invertColor(colors[i].hex), size, alpha));
+            }
+            return points;
+
+        }
+    }
 };
